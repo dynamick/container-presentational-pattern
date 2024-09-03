c04c86527e77338940cd82479b4065036466db68
Michele Gobbi <info@dynamick.it>
2024-09-03 12:28:00 +0000

    added some info texts

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Component / Presentational pattern</h1>
+      <h2>This is a simple Component/Presentational design pattern example.</h2>
+      <p>See <a href="https://www.patterns.dev/react/presentational-container-pattern">Patterns.dev</a> for more infos.</p>
       <DogImages />
     </div>
   );
